129fedb3c96a98506fa7ae13310d4dd0ee61ba32
Simon ROUSSEAU <simon.rousseau@total.com>
2022-01-13 16:16:36 +0100

    added random name API call

--- get-welcome/index.js
+++ get-welcome/index.js
@@ -1,9 +1,12 @@
 module.exports = async function (context, req) {
+    const axios = require("axios");
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
+    //const name = (req.query.name || (req.body && req.body.name));
+    const name = await axios.get("https://randomuser.me/api/");
+
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hello, " + name.data.results[0].name.first + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
